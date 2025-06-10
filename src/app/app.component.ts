9bf8960f8aacb922e7fdaa21d6b26e187db4dcd4
Altina Hoti <altina.hoti@smartits.de>
2025-06-10 11:15:50 +0200

    Add the commponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+
+import { CustomInputComponent } from './shared/custom-input/custom-input.component';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CustomInputComponent],
+  //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+  styleUrls: ['./app.component.css']
+  })
 export class AppComponent {
-  title = 'Angular-project';
+  //title = 'Angular-project';
+  faSearch = faSearch;
 }
---
+++ src/app/shared/custom-input/custom-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, input, Input } from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector:'app-custom-input',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.css']
+
+})
+export class CustomInputComponent {
+  @Input () placeholder: string = '';
+  @Input() icon?: IconDefinition;
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input () color: string = '#0000';
+}
